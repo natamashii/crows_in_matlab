32d1c7ba507ab14f3335b5b33f8c2e138e23fe0b
natamashii <f-natalie@gmx.net>
2025-09-14 19:09:04 +0200

    make things more flexible, fix rec_times plot for split. start lin_regress more

--- Toolbox/lin_regress.m
+++ Toolbox/lin_regress.m
@@ -1,10 +1,21 @@
-function [penis] = lin_regress()
+function [penis] = lin_regress(ind_data, to_split, patterns)
 
 % function to compute linear regression of all data that should be compared
 % to each other
 
 % NOTES
 % include p value shit + effect size (R^2 ???)
+% if to_split, then ind_data must be a 1x2 cell with ind data of cases
+
+if to_split
+    ind_data_1 = ind_data{1};
+    ind_data_2 = ind_data{2};
+end
+
+% iterate over patterns
+for pattern = 1:length(patterns)
+
+end
 
 
 end
--- analysis_sem_errbars.m
+++ analysis_sem_errbars.m
@@ -6,7 +6,7 @@ close all
 
 % TODO
 % DONE divide standard/control stuff, save it separately (incl statistic shit from that)
-% generalize standard/control to Jello/Uri
+% DONE generalize standard/control to Jello/Uri
 % implement statistics
 % DONE rewrite analysis stuff as functions
 % bootstrapping: more bootstrap statistic? some p value or such stuff?
@@ -18,11 +18,21 @@ close all
 % DONE rewrite bootstrapping as one function
 % DONE make plots with individual dots in background (like fish graphics)
 % save data (individual stuff and mean stuff)
-% make fig size variable
+% make fig size variable (robust for each device used)
 % maybe time for birds
 % imporve single plot
-% regression analysis: add computing in function + plot regression curve
-% separetly in a function
+% regression analysis: add computing in function + plot regression curve separetly in a function
+% DONE rewrite standard/control & jello/uri stuff into one case
+% DONE different colours for uri & jello comparison thatn for standard & control
+% comparison of birds different experiments exp 1
+% DONE make prompt asking adaptable
+% write one script to load all stuff to make it presentable to lena
+% in stimpattern_generation.m change input to case insensitive with strcmpi();
+% change ind dot colours to be a bit different than the avg data (overlays with box plots...)
+% maybe combine plot_stuff with plot_sc somehow....
+% add error type to legend in each plot function
+% for plotting: extract dot_alpha & marker factor & box alpha & boxwidth factor for all functions
+% Anova S/C and J/U: maybe pre process RTs: remove too quick trials & remove everything that is +- 3* MAD of median
 
 
 % Note
@@ -44,16 +54,19 @@ close all
 
 % Note: 9 in all columns for one row = abundance by bird
 
-% Pre allocation
-statistics = table();
 
-% Pre Definition
+%% Pre Definition
 
 who_analysis = {'humans\'; 'jello\'; 'uri\'; 'birds\'};
 what_analysis = {'Performance'; 'Response Frequency'; 'Reaction Times'};
 calc_type = {'Mean', 'Median'};
 err_type = {'STD', 'SEM', 'CI'};
 focus_type = {'Overall', 'Matches', 'Single'};
+
+to_save = true; % if result shall be saved
+to_correct = false; % if response matrices shall be corrected
+to_split_sc = false;    % if to compare standard & control conditions
+to_split_ju = false;    % if to compare Jello's & Uri's data
 
 % crows: 1 = exp 1 100ms, 2 = exp 1 300ms, 3 = exp 1 50ms, 4 = exp 2 50ms
 % humans: 1 = exp 1 50ms, 2 = exp 2 50ms, 3 = exp 3 50ms
@@ -65,6 +78,35 @@ numerosities = [3, 4, 5, 6, 7; % sample
     6, 7, 8, 9, 10]';  % test 3 numbers
 patterns = {'P1', 'P2', 'P3'};
 
+% for Plotting
+colours_pattern = ...
+    {[0.8008 0.2578 0.7266]; [0.1445 0.4336 0.2070]; ...
+    [0.1211 0.5195 0.6289]};    % colours for patterns
+colours_numbers = {[0 0.4460 0.7410]; [0.8500 0.3250 0.0980]; ...
+    [0.9290 0.6940 0.1250]; [0.3010 0.7450 0.9330]; ...
+    [0.6350 0.0780 0.1840]};    % colours for samples
+colours_S_C = ...
+    {[0.0660 0.4430 0.7450]; [0.5210 0.0860 0.8190]};   % Standard/Control
+colours_J_U = ...
+    {[0.0000 0.4080 0.7410]; [0.9690 0.3650 0.8000]};   % Jello/Uri
+format = 'svg'; % figure save format
+fig_title = '';
+
+plot_font = 12;
+plot_pos = [21 29.7];   % default PaperPosition size of figure
+mrksz = 10;
+linewidth = 2;
+plot_font = 14;
+capsize = 10;
+jitterwidth = 0.25;
+linestyle = "none";
+
+% values for computing confidence interval
+n_boot = 10000;
+confidence_level = 95;      % For a 95% CI
+alpha = 100 - confidence_level;
+
+%% Set specifics 
 % prompt to ask who to analyse
 prompt = ['Who do you wish to plot? ' ...
     ' \n 1 - humans ' ...
@@ -72,6 +114,17 @@ prompt = ['Who do you wish to plot? ' ...
     ' \n 3 - Uri ' ...
     ' \n 4 - Crows (Jello + Uri) '];
 who_analysis = who_analysis{str2double(input(prompt, "s"))};
+
+% If Jello and Uri shall be compared
+if strcmp(who_analysis, 'birds\')
+    to_split_ju = true;
+% Prompt to ask if division into Standard & Control Conditions
+else
+    prompt = 'Split data into Standard & Control? (y/n) ';
+    if strcmpi(input(prompt, "s"), 'y')
+        to_split_sc = true;
+    end
+end
 
 % prompt to ask for experiment
 if strcmp(who_analysis, 'humans\')
@@ -99,26 +152,6 @@ prompt = ['What do you wish to analyse? '...
     '\n 3 - Reaction Time '];
 what_analysis = what_analysis{str2double(input(prompt, "s"))};
 
-% prompt to ask what to analyse & to plot
-prompt = ['What do you wish to plot? ' ...
-    ' \n 1 - Mean ' ...
-    ' \n 2 - Median '];
-calc_type = calc_type{input(prompt)};
-
-% prompt to ask for error type to plot
-prompt = ['With what type of error? ' ...
-    ' \n 1 - STD ' ...
-    ' \n 2 - SEM ' ...
-    ' \n 3 - CI '];
-err_type = err_type{input(prompt)};
-
-% prompt to ask for which combination to plot
-prompt = ['What exactly do you wanna look at? ' ...
-    ' \n 1 - Overall (Match + Non-Match) ' ...
-    ' \n 2 - Matches ' ...
-    ' \n 3 - Behavioural Curves '];
-focus_type = focus_type{input(prompt)};
-
 % Path definition
 base_path = 'D:\MasterThesis\analysis\data\';
 figure_path = ['D:\MasterThesis\figures\progress_since_250902\' who_analysis '\'];
@@ -130,84 +163,108 @@ filelist_spk = dir(figure_path);  % list of all data & subfolders
 subfolders = filelist_spk([filelist_spk(:).isdir]); % extract subfolders
 subfolders = {subfolders(3:end).name};  % list of subfolder names (experiments)
 
-to_save = true; % if result shall be saved
-to_correct = false; % if response matrices shall be corrected
-s_c = true;
-
-% for Plotting
-colours_pattern = ...
-    {[0.8008 0.2578 0.7266]; [0.1445 0.4336 0.2070]; [0.1211 0.5195 0.6289]};
-colours_numbers = {[0 0.4460 0.7410]; [0.8500 0.3250 0.0980]; ...
-    [0.9290 0.6940 0.1250]; [0.3010 0.7450 0.9330]; [0.6350 0.0780 0.1840]};
-colours_S_C = {[0.0660 0.4430 0.7450]; [0.5210 0.0860 0.8190]};
-format = 'svg';
-fig_title = '';
-
-plot_font = 12;
-plot_pos = [21 29.7];   % default PaperPosition size of figure
-mrksz = 10;
-linewidth = 2;
-plot_font = 14;
-in_detail = false;
-capsize = 10;
-jitterwidth = 0.25;
-linestyle = "none";
-
-n_boot = 10000;
-confidence_level = 95;      % For a 95% CI
-alpha = 100 - confidence_level;
-
-% Correct Response Matrix
+%% Correct Response Matrix
 if to_correct
     corr_resp(rsp_mat_folderpath, spk_folderpath, who_analysis, ...
         curr_exp, numerosities);
 end
 
-% Sum Average Performance for each Pattern
-if s_c
-    [performances_s, performances_c, ...
-        resp_freq_s, resp_freq_c, rec_times_s, rec_times_c] = ...
+%% Extract Data
+% Comparison of Jello & Uri
+if to_split_ju
+    % get data for Jello
+    [performances_1, resp_freq_1, rec_times_1] = ...
+        sort_behav(rsp_mat_folderpath, 'jello\', ...
+        curr_exp, numerosities, patterns);
+
+    % get data for Uri
+    [performances_2, resp_freq_2, rec_times_2] = ...
+        sort_behav(rsp_mat_folderpath, 'uri\', ...
+        curr_exp, numerosities, patterns);
+
+    % set some variables for later plotting
+    factors_plot = {'Jello', 'Uri'};
+    factors_stats = {'J', 'U'};
+    fig_name_part = '_JelloUri_';
+    colours_split = colours_J_U;
+
+    % set focus type to 'Matches'
+    focus_type = focus_type{2};
+
+% Comparison of Standard % Control Conditions
+elseif to_split_sc
+    % get data for standard & control conditions
+    [performances_1, performances_2, ...
+        resp_freq_1, resp_freq_2, rec_times_1, rec_times_2] = ...
         stand_cont(rsp_mat_folderpath, who_analysis, ...
         curr_exp, numerosities, patterns);
+
+    % set some variables for later plotting
+    factors_plot = {'Standard', 'Control'};
+    factors_stats = {'S', 'C'};
+    fig_name_part = '_StandCont_';
+    colours_split = colours_S_C;
+
+    % set focus type to 'Matches'
+    focus_type = focus_type{2};
+
+% No explicit comparison
 else
+    % prompt to ask for which combination to plot
+    prompt = ['What exactly do you wanna look at? ' ...
+        ' \n 1 - Overall (Match + Non-Match) ' ...
+        ' \n 2 - Matches ' ...
+        ' \n 3 - Behavioural Curves '];
+    focus_type = focus_type{input(prompt)};
+    
+    % get data
     [performances, resp_freq, rec_times] = ...
         sort_behav(rsp_mat_folderpath, who_analysis, ...
         curr_exp, numerosities, patterns);
 end
 
-% Get data depending on what to analyse
+%% Get data depending on what to analyse
 switch what_analysis
     case 'Performance'
-        if s_c
-            [avg_data_s, err_data_s] = ...
-                calc_behav(performances_s, what_analysis, ...
+        calc_type = calc_type{1};   % Mean
+        err_type = err_type{2};     % SEM
+        if to_split_ju | to_split_sc
+            % Standard/Jello
+            [avg_data_1, err_data_1] = ...
+                calc_behav(performances_1, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            [avg_data_c, err_data_c] = ...
-                calc_behav(performances_c, what_analysis, ...
+            ind_data_1 = performances_1;
+            % Control/Uri
+            [avg_data_2, err_data_2] = ...
+                calc_behav(performances_2, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            ind_data_s = performances_s;
-            ind_data_c = performances_c;
-        else
+            ind_data_2 = performances_2;
+        else 
             [avg_data, err_data] = ...
                 calc_behav(performances, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
             ind_data = performances;
         end
+
     case 'Response Frequency'
-        if s_c
-            [avg_data_s, err_data_s] = ...
-                calc_behav(resp_freq_s, what_analysis, ...
+        calc_type = calc_type{1};   % Mean
+        err_type = err_type{2};     % SEM
+        if to_split_ju | to_split_sc
+            % Standard/Jello
+            [avg_data_1, err_data_1] = ...
+                calc_behav(resp_freq_1, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            [avg_data_c, err_data_c] = ...
-                calc_behav(resp_freq_c, what_analysis, ...
+            ind_data_1 = resp_freq_1;
+            % Control/Uri
+            [avg_data_2, err_data_2] = ...
+                calc_behav(resp_freq_2, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            ind_data_s = resp_freq_s;
-            ind_data_c = resp_freq_c;
+            ind_data_2 = resp_freq_2;
         else
             [avg_data, err_data] = ...
                 calc_behav(resp_freq, what_analysis, ...
@@ -215,18 +272,23 @@ switch what_analysis
                 numerosities, n_boot, alpha, focus_type);
             ind_data = resp_freq;
         end
+
     case 'Reaction Times'
-        if s_c
-            [avg_data_s, err_data_s] = ...
-                calc_behav(rec_times_s, what_analysis, ...
+        calc_type = calc_type{2};   % Median
+        err_type = err_type{1};     % STD, although not necessary (boxplot)
+        if to_split_ju | to_split_sc
+            % Standard/Jello
+            [avg_data_1, err_data_1] = ...
+                calc_behav(rec_times_1, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            [avg_data_c, err_data_c] = ...
-                calc_behav(rec_times_c, what_analysis, ...
+            ind_data_1 = rec_times_1;
+            % Control/Uri
+            [avg_data_2, err_data_2] = ...
+                calc_behav(rec_times_2, what_analysis, ...
                 calc_type, err_type, patterns, ...
                 numerosities, n_boot, alpha, focus_type);
-            ind_data_s = rec_times_s;
-            ind_data_c = rec_times_c;
+            ind_data_2 = rec_times_2;
         else
             [avg_data, err_data] = ...
                 calc_behav(rec_times, what_analysis, ...
@@ -235,47 +297,43 @@ switch what_analysis
             ind_data = rec_times;
         end
 end
-    
+
+%% Statistics
+if to_split_ju | to_split_sc
+    statistics = anova_sc(performances_1, performances_2, ...
+        resp_freq_1, resp_freq_2, rec_times_1, rec_times_2, patterns, ...
+        numerosities, stats_path, subfolders{curr_exp}, ...
+        factors_stats, ['statistics_' fig_name_part(1:end - 1)]);
+end
+
+
+   
+%% Plotting
 % Plot specificites depending on what to analyse
 if strcmp(focus_type, 'Single')
     linestyle = "--";
     plot_pos = [50 29.7];
 end
 
-% Plot
-if s_c
+% if comparison wished
+if to_split_ju | to_split_sc
     plot_pos = [29.7 50];
-    fig = plot_s_c(numerosities, ind_data_s, ind_data_c, ...
-        avg_data_s, avg_data_c, err_data_s, err_data_c, what_analysis, ...
-        who_analysis(1:end-1), calc_type, experiments{curr_exp}, ...
+    fig = plot_s_c(numerosities, ind_data_1, ind_data_2, ...
+        avg_data_1, avg_data_2, err_data_1, err_data_2, what_analysis, ...
+        who_analysis(1:end - 1), calc_type, experiments{curr_exp}, ...
         patterns, err_type, jitterwidth, ...
-        colours_S_C, mrksz, plot_font, plot_pos, ...
-        linewidth, capsize, linestyle);
-    fig_name = [focus_type '_StandCont_' calc_type '_' err_type '_' ...
+        colours_split, mrksz, plot_font, plot_pos, ...
+        linewidth, capsize, linestyle, factors_plot);
+    fig_name = [focus_type fig_name_part calc_type '_' err_type '_' ...
         what_analysis '.' format];
 else
     fig = plot_stuff(ind_data, avg_data, err_data, numerosities, ...
         patterns, calc_type, err_type, what_analysis, ...
-        who_analysis(1:end-1), ...
+        who_analysis(1:end - 1), ...
         experiments{curr_exp}, plot_font, colours_pattern, plot_pos, ...
         linewidth, linestyle, mrksz, capsize, jitterwidth, focus_type);
     fig_name = [focus_type '_' calc_type '_' err_type '_' ...
         what_analysis '.' format];
-end
-
-% Add statistics
-if s_c
-    statistics_s_c = anova_sc(performances_s, performances_c, ...
-        resp_freq_s, resp_freq_c, rec_times_s, rec_times_c, patterns, ...
-        numerosities, stats_path, subfolders{curr_exp}, ...
-        {'Standard', 'Control'}, 'statistics_standard_control');
-end
-
-if strcmp(what_analysis, 'birds\')
-    statistics_s_c = anova_sc(performances_s, performances_c, ...
-        resp_freq_s, resp_freq_c, rec_times_s, rec_times_c, patterns, ...
-        numerosities, stats_path, subfolders{curr_exp}, ...
-        {'Jello', 'Uri'}, 'statistics_jello_uri');
 end
 
 % save figure
@@ -283,4 +341,6 @@ if to_save
     adapt_path = [figure_path '\' subfolders{curr_exp} '\'];
     saveas(fig, [adapt_path, fig_name], format)
 end
+
+
 
